953344d0bfb8c338ad5711a82b70db6cb1c887e0
AlexRg32 <alexrg32@outlook.es>
2025-05-05 14:23:05 +0200

    Añadida documentacion openAPI
    A falta de que funcione

--- src/main/java/com/backend/Tenis/TenisApplication.java
+++ src/main/java/com/backend/Tenis/TenisApplication.java
@@ -1,5 +1,9 @@
 package com.backend.Tenis;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -15,6 +19,19 @@ public class TenisApplication {
 	@Bean
 	public ModelMapper modelMapper() {
 		return new ModelMapper();
+	}
+
+	@Bean
+	public OpenAPI customOpenAPI() {
+		return new OpenAPI()
+				.components(new Components())
+				.info(new Info().title("Cine API")
+						.description("Ejemplo de API REST")
+						.contact(new Contact()
+								.name("Trino Gómez")
+								.email("trino@hotmail.com")
+								.url("https://portal.edu.gva.es/iesmaciaabela/l"))
+						.version("1.0"));
 	}
 
 }
